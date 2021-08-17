c59819fb9d5908eec3d41701420fe3a4cea3ab8b
MEaston1 <mattheweaston21@gmail.com>
2021-08-17 15:18:49 +0100

    Navigation component
    Addition of a navigation component connecting placeholder fragments to form the base of the design of the app.

--- app/src/main/java/com/view/imgurviewer/ui/ImgurActivity.kt
+++ app/src/main/java/com/view/imgurviewer/ui/ImgurActivity.kt
@@ -2,11 +2,16 @@ package com.view.imgurviewer.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.view.imgurviewer.R
+import kotlinx.android.synthetic.main.activity_imgur.*
 
 class ImgurActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imgur)
+
+        bottomNavigationView.setupWithNavController(imgurViewerNavFragment.findNavController())
     }
 }
