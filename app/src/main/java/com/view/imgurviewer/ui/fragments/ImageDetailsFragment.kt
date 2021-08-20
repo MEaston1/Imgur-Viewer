88b9b51e9a39a7ae5c10bdb3820848c789481774
MEaston1 <mattheweaston21@gmail.com>
2021-08-20 11:26:29 +0100

    Image Details Fragment update
    The image details fragment is laid out with all of it's relevant information.

--- app/src/main/java/com/view/imgurviewer/ui/fragments/ImageDetailsFragment.kt
+++ app/src/main/java/com/view/imgurviewer/ui/fragments/ImageDetailsFragment.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.google.android.material.snackbar.Snackbar
 import com.view.imgurviewer.R
 import com.view.imgurviewer.ui.ImagesViewModel
 import com.view.imgurviewer.ui.ImgurActivity
 import kotlinx.android.synthetic.main.fragment_image_details.*
+import kotlinx.android.synthetic.main.fragment_image_preview.view.*
 
 class ImageDetailsFragment : Fragment(R.layout.fragment_favourited_images) {
     lateinit var viewModel: ImagesViewModel
@@ -18,6 +20,9 @@ class ImageDetailsFragment : Fragment(R.layout.fragment_favourited_images) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as ImgurActivity).viewModel
         val image = args.image
+        imageName.text = image.title
+        imageDescription.text = image.description as CharSequence?
+        Glide.with(this).load(image.link).into(displayedImage)
 
         fab.setOnClickListener {
             viewModel.favouriteImage(image)
